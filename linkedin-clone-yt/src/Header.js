5ddba8d35b4ae6730c7b8838b4508775ea14b801
crazybarber90 <pepy90aa@gmail.com>
2022-02-07 23:49:41 +0100

    finished header

--- linkedin-clone-yt/src/Header.js
+++ linkedin-clone-yt/src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from "@mui/icons-material/Search";
 import HeaderOption from "./HeaderOption";
 import HomeIcon from "@mui/icons-material/Home";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcons from "@mui/icons-material/Notifications";
 
 function Header() {
   return (
@@ -21,6 +24,10 @@ function Header() {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcons} title="Notifications" />
+        <HeaderOption avatar="https://imgur.com/a/K1BGfnp" title="me" />
       </div>
     </div>
   );
