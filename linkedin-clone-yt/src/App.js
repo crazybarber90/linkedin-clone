98720488da8b3dd8a1a92a9c49344fb725cea6d4
crazybarber90 <pepy90aa@gmail.com>
2022-02-07 11:56:11 +0100

    first commit LI

---
+++ linkedin-clone-yt/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+
+      {/* App Body */}
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+
+      {/* STAO SAM NA 50:56   https://www.youtube.com/watch?v=QaYts9sPmcY
+      https://icons8.com/icons/ IKONICE
+      */}
+    </div>
+  );
+}
+export default App;
---
+++ linkedin-clone-yt/src/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://img.icons8.com/fluency/48/000000/linkedin.png"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
